a0855876ffdec1bbb244b9ed672cffae2fb201a2
Iforgotmybrain <47312700+Iforgotmybrain@users.noreply.github.com>
2019-05-19 01:34:57 -0400

    Added Cafe dialog and introduced a new character
    Added most of the Cafe dialog, which introduces a new character, Holly the Fox. Friend of the PC from high school.
    
    Cafe area is still a WIP as I mess around with ways of implementing dialog and trans-
    sistions
    
    Imported pygame for future use

--- Base.py
+++ Base.py
@@ -4,10 +4,14 @@
 
 import time
 import secrets
+import pygame
 
+from pygame.locals import  *
 # Room functions. This is the bulk of the game. Not sure if this is the best way to program this type of game
 # But this seems to be the organized way, and the easiest way to edit things if needed.
 
+
+# Idea to possibly implement: Replace the time.sleep commands with input commands to give the player more control.
 
 class PlayerStats:
     def __init__(self, race, sex, name):
@@ -196,13 +200,86 @@ class ToriesCafe:
         time.sleep(5)
         print("It’s a popular place amongst the younger crowd. The place has a modern aesthetic with colorful furniture and ample natural lighting giving the place a cheery vibe.")
         time.sleep(5)
-        print("They’re famous for their fantastic wraps, and also have some pretty soups.")
+        print("They’re famous for their fantastic wraps, and also have some pretty good soups.")
         time.sleep(3)
         print("Looking around you see the line to order, it’s a bit after lunch so there isn’t much of a wait. You also see a familiar face sitting down at one of the tables")
         time.sleep(5)
         cafedecision = input("Will you order first or go and say hi to the familiar face? ").lower()
         if cafedecision in ['order', 'eat', ]:
-            print("Placeholder text")
+            print("You decide to order some food before going to say hi.")
+            time.sleep(5)
+            print("The line moves quick and before you know it’s your turn to order.")
+            print("You decide to order your usual combo; a tuna wrap with a bag of chips and a drink. Not a bad deal for $4!")
+            print("Having ordered your food, you head over to the table of Holly, a friend of yours from high school.") # Experimenting with a new text printing system
+            input()
+            print("Holly is a vixen, no, not like that. In the literal sense. You’ve known her since high school and while you haven’t really been in contact much since college, you still consider her to be a friend, albeit a distant one.")
+            input()
+            print("You exchange greetings with Holly and start convesing. There’s a lot of catching up to do, you follow each other on social media but of course that isn’t a replacement for proper conversation.")
+            print('"Hey"', player_info.name,'.' '"Its been a while hasn\'t it? Last time I saw you was when we went to the amusement park your sophomore year of college with a bunch of other high school friends. And the last I saw you regularly was back before you went off to college in 2014!"')
+            input()
+            print("You fill in some details about what you’ve been up too since that amusement park trip. Detailing your current living situation with Sasha and Jacob, as well as talking about your various college antics")
+            print("You ask what Holly’s been up too since then, a faint look of worry and disappointment fills her face as she describes her falling out from college")
+            input()
+            print('"Yeah, I dropped out of college at the end of 2016. Believe it or not it wasn’t because of my grades, I just wasn’t enjoying college. I know that’s a pretty shit reason to drop out, but I just couldn’t see myself doing another 2 years there. Nor could I see myself being a marketer for the rest of my life"')
+            print('"Like I said, my grades weren’t bad enough too have them kick me out, but they weren’t great either. I was holding about a 2.5 (out of 4) GPA"')
+            input()
+            print("You nod in agreement, remembering how many times you second guessed your major choice throughout college")
+            input()
+            print('"And as far as being a marketer goes, I knew I would have hated it and the corporate culture that surrounds it. Having to stick up to execs, deliver presentations on ideas that would ultimately be ignored by the project managers and higher-ups. Dealing with the petty workplace drama… That just wasn’t for me."')
+            input()
+            print("You can definitely see yourself understanding Holly’s decision, thinking back to all the workplace drama and rejected proposals you’ve dealt with…")
+            print("You ask Holly what she’s doing for work since she dropped out of college, she offers a surprising answer.")
+            input()
+            print('"I draw art for a living now! I’ve always been interested in drawing before, I’m sure you remember some of my drawings from back in high school. I never really thought of it as a legitimate career path, but I’ve managed to find a niche that pays a decent amount of money through commissions."')
+            print('"I love it. The customer tells me what they want, I draw it, and we go on our way. There’s (typically) no bullshit, and no one else telling me what to do."')
+            input()
+            print('"So, what have you been doing since graduating college? I know you finished college with a couple internships and a great GPA, so that’s probably gotten you somewhere right?"')
+            input()
+            print("You explain that you used to work for a local big business, but eventually quit for various reasons, including reasons she stated, like project managers ignoring ideas.")
+            print("You state that you had luckily saved up enough money to live comfortably for around 2 years before quitting. You talk about how you now do freelance work off and on, not unlike what Holly does. It helps keep a steady flow of money coming into your bank account. And with living expenses being split between three people, you really don’t need much money to support your lifestyle.")
+            input()
+            print("'Man, that’s fantastic. Hearing stuff like that almost makes me wish I would have stayed in college. I couldn’t imagine being able to have a savings large enough to live on for 2 years. Let alone being able to amass that much money by only working for a year and half!'")
+            print("'Still, like I said, I enjoy my work and I wouldn’t want it any other way.'")
+            input()
+            print("Well", player_info.name, "it’s been fantastic talking but I’ve got a yoga class coming up in a half hour so I’ve gotta run. Hopefully I’ll see you around.")
+            print("You say goodbye to Holly and decide to head home for the day")
+            input()
+            hallway()
+        elif cafedecision in ['talk', 't', 'face' 'hi']:
+            print("You decide to first go over and say hi to Holly.")
+            print("Holly is a vixen, no, not like that. In the literal sense. You’ve known her since high school and while you haven’t really been in contact much since college, you still consider her to be a friend, albeit a distant one.")
+            input()
+            print("You exchange greetings with Holly and start convesing. There’s a lot of catching up to do, you follow each other on social media but of course that isn’t a replacement for proper conversation.")
+            print('"Hey"', player_info.name, "." "'Last time I saw you was when we went to the amusement park your sophomore year of college with a bunch of other high school friends. And the last I saw you regularly was back before you went off to college in 2014!'")
+            input()
+            print("You fill in some details about what you’ve been up too since that amusement park trip. Detailing your current living situation with Sasha and Jacob, as well as talking about your various college antics")
+            print("You ask what Holly’s been up too since then, a faint look of worry and disappointment fills her face as she describes her falling out from college")
+            input()
+            print("'Yeah, I dropped out of college at the end of 2016. Believe it or not it wasn’t because of my grades, I just wasn’t enjoying college. I know that’s a pretty shit reason to drop out, but I just couldn’t see myself doing another 2 years there. Nor could I see myself being a marketer for the rest of my life'")
+            print("'Like I said, my grades weren’t bad enough too have them kick me out, but they weren’t great either. I was holding about a 2.5 (out of 4) GPA'")
+            input()
+            print("You nod in agreement, remembering how many times you second guessed your major choice throughout college")
+            input()
+            print("'And as far as being a marketer goes, I knew I would have hated it and the corporate culture that surrounds it. Having to stick up to execs, deliver presentations on ideas that would ultimately be ignored by the project managers and higher-ups. Dealing with the petty workplace drama… That just wasn’t for me.'")
+            input()
+            print("You can definitely see yourself understanding Holly’s decision, thinking back at all the workplace drama and rejected proposals you’ve dealt with…")
+            print("You ask Holly what she’s doing for work since she dropped out of college, she offers a surprising answer")
+            input()
+            print("'I draw art for a living now! I’ve always been interested in drawing before, I’m sure you remember some of my drawings from back in high school. I never really thought of it as a legitimate career path, but I’ve managed to find a niche that pays a decent amount of money through commissions.'")
+            print("'I love it. The customer tells me what they want, I draw it, and we go on our way. There’s (typically) no bullshit, and no one else telling me what to do.'")
+            input()
+            print("'So, what have been doing since graduating college? I know you finished with couple internships and a great GPA, so that’s had to have gotten you somewhere right?'")
+            input()
+            print("You explain that you used to work for a local big business, but eventually quit for various reasons, including reasons she stated, like project managers ignoring ideas.")
+            print("You state that you had luckily saved up enough money to live comfortably for around 2 years before quitting. You talk about how you now do freelance work off and on, not unlike what Holly does. It helps keep a steady flow of money coming into your bank account. And with living expenses being split between three people, you really don’t need much money to support your lifestyle.")
+            input()
+            print("'Man, that’s fantastic. Hearing stuff like that almost makes me wish I would have stayed in college. I couldn’t imagine being able to have a savings large enough to live on for 2 years. Let alone being able to amass that much money by only working for a year and half!'")
+            print("'Still, like I said, I enjoy my work and I wouldn’t want it any other way.'")
+            input()
+            print("'Well'", player_info.name, "'it’s been fantastic talking but I’ve got a yoga class coming up in a half hour so I’ve gotta run. Hopefully I’ll see you around.'")
+            print("You say goodbye to Holly and decide to head home for the day")
+            input()
+            hallway()
 
 
 class SycamorePark:
